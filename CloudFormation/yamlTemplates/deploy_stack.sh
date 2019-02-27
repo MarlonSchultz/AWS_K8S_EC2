8d2d7e7d020fe5b81a708987cc86ffbdcfbf6666
marlon <marlon.schultz@mayflower.de>
2019-02-27 10:42:41 +0100

    changed ec2 instances to have ubuntu as base; added cloud-init stuff --> not working as of yet

--- CloudFormation/yamlTemplates/deploy_stack.sh
+++ CloudFormation/yamlTemplates/deploy_stack.sh
@@ -9,7 +9,8 @@ echo "Waiting for stack to complete"
 aws cloudformation wait stack-create-complete --stack-name $STACK_PREFIX-networking --profile=$AWS_PROFILE
 
 #jumphost
-aws cloudformation create-stack --stack-name $STACK_PREFIX-sg-jumphost --template-body file://jumphost/securityGroupJumpHost.yaml --profile=$AWS_PROFILE --parameters ParameterKey=NetworkStackNameParameter,ParameterValue=$STACK_PREFIX-networking
+aws cloudformation create-stack --stack-name $STACK_PREFIX-sg-jumphost --template-body file://jumphost/securityGroupJumpHost.yaml --profile=$AWS_PROFILE \
+--parameters ParameterKey=NetworkStackNameParameter,ParameterValue=$STACK_PREFIX-networking
 echo "Waiting for stack to complete"
 aws cloudformation wait stack-create-complete --stack-name $STACK_PREFIX-sg-jumphost --profile=$AWS_PROFILE
 
@@ -21,3 +22,14 @@ ParameterKey=JumpHostKey,ParameterValue=$JUMPHOSTKEY
 
 echo "Waiting for stack to complete"
 aws cloudformation wait stack-create-complete --stack-name $STACK_PREFIX-jumphost --profile=$AWS_PROFILE
+
+# ec2 instances
+aws cloudformation create-stack --stack-name $STACK_PREFIX-ec2-instances --template-body file://ec2_instances/ec2_instances.yaml --profile=$AWS_PROFILE \
+--parameters \
+ParameterKey=PrivateSubnetOne,ParameterValue=$STACK_PREFIX-networking-PrivSubNet1 \
+ParameterKey=PrivateSubnetTwo,ParameterValue=$STACK_PREFIX-networking-PrivSubNet2 \
+ParameterKey=SSHKeyName,ParameterValue=$JUMPHOSTKEY \
+ParameterKey=NetworkStackNameParameter,ParameterValue=$STACK_PREFIX-networking
+
+echo "Waiting for stack to complete"
+aws cloudformation wait stack-create-complete --stack-name $STACK_PREFIX-ec2-instances --profile=$AWS_PROFILE
